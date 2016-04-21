12b702911709d92cab3206acfb57f625fb1e0e8e
amun33 <amunn33@gmail,com>
2016-04-21 13:04:43 -0400

    Made changes for default values on db

--- createdb.sql
+++ createdb.sql
@@ -1,6 +1,6 @@
 CREATE TABLE User (
   UserID Integer PRIMARY KEY,
-  Name Text ,
+  Name Text NOT NULL,
   RoleID REFERENCES Role(RoleID)
 );
 
@@ -20,18 +20,18 @@ CREATE TABLE Coaches (
 
 CREATE TABLE Team (
   TeamID Integer PRIMARY KEY,
-  Name Text
+  Name Text NOT NULL
 );
 
 CREATE TABLE Role (
   RoleID Integer PRIMARY KEY,
-  Title Text ,
+  Title Text NOT NULL,
   Description Text
 );
 
 CREATE TABLE Event (
   EventID Integer PRIMARY KEY,
-  DateTime Text,
+  DateTime DateTime NOT NULL,
   Location Text,
   TeamID REFERENCES Team(TeamID),
   TypeID REFERENCES Type(TypeID)
@@ -39,21 +39,23 @@ CREATE TABLE Event (
 
 CREATE TABLE Event_Type (
   TypeID Integer PRIMARY KEY,
-  Description Text
+  Description Text NOT NULL
 );
 
 CREATE TABLE Notified_For (
   ContactID REFERENCES Contact(ContactID),
   TypeID REFERENCES Event_Type(TypeID),
-  NotificationTime Text,
+  NotificationTime Text NOT NULL,
   PRIMARY KEY (ContactID, TypeID)
 );
 
 CREATE TABLE Contact (
-  ContactID Integer PRIMARY KEY,
-  PhoneEmail Text,
-  ContactType Text
+  ContactID Integer,
+  Contact Text NOT NULL,
+  isPhone Boolean NOT NULL,
+  PRIMARY KEY (ContactID, Contact)
 );
+
 
 CREATE TABLE Uses (
   UserID REFERENCES User(UserID),
@@ -66,5 +68,5 @@ CREATE TABLE Equipment (
   LoanerID REFERENCES User(UserID),
   BorrowerID REFERENCES User(UserID),
   Kind Text,
-  LoanDate Text
+  LoanDate DateTime DEFAULT (datetime('now', 'localtime'))
 );
