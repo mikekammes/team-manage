4c9e2cb5bd5e6daec3a57bfc0427dd48ade05977
Michael <meanteen13@gmail.com>
2016-04-21 13:05:22 -0400

    add bootstrap and a base template

--- team-manage.py
+++ team-manage.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
+import datetime
+
+now = datetime.datetime.now()
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('base.html', year=now.year)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
