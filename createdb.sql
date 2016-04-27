52f987bce2159434efda6adf7f2c77944e2a6de0
amun33 <amunn33@gmail,com>
2016-04-26 20:49:45 -0400

    Changing SQL statements

--- createdb.sql
+++ createdb.sql
@@ -1,21 +1,17 @@
-CREATE TABLE User (
-  UserID Integer PRIMARY KEY,
-  Name Text NOT NULL,
-  RoleID REFERENCES Role(RoleID)
-);
-
+DROP TABLE IF EXISTS Plays_For;
 CREATE TABLE Plays_For (
-  UserID REFERENCES User(UserID),
-  TeamID REFERENCES Team(TeamID),
+  UserID INTEGER REFERENCES User(UserID),
+  TeamID INTEGER REFERENCES Team(TeamID),
   Number Integer,
   Position Text,
   Joined Boolean,
   PRIMARY KEY (UserID, TeamID)
 );
 
+DROP TABLE IF EXISTS Coaches;
 CREATE TABLE Coaches (
-  UserID REFERENCES User(UserID),
-  TeamID REFERENCES Team(TeamID),
+  UserID INTEGER REFERENCES User(UserID),
+  TeamID INTEGER REFERENCES Team(TeamID),
   PRIMARY KEY (UserID, TeamID)
 );
 
@@ -24,12 +20,7 @@ CREATE TABLE Team (
   Name Text NOT NULL
 );
 
-CREATE TABLE Role (
-  RoleID Integer PRIMARY KEY,
-  Title Text NOT NULL,
-  Description Text
-);
-
+DROP TABLE IF EXISTS Event;
 CREATE TABLE Event (
   EventID Integer PRIMARY KEY,
   DateTime DateTime NOT NULL,
@@ -38,14 +29,17 @@ CREATE TABLE Event (
   TypeID REFERENCES Type(TypeID)
 );
 
+
+DROP TABLE IF EXISTS Event_Type;
 CREATE TABLE Event_Type (
   TypeID Integer PRIMARY KEY,
   Description Text NOT NULL
 );
 
+DROP TABLE IF EXISTS Notified_For;
 CREATE TABLE Notified_For (
-  ContactID REFERENCES Contact(ContactID),
-  TypeID REFERENCES Event_Type(TypeID),
+  ContactID INTEGER REFERENCES Contact(ContactID),
+  TypeID INTEGER REFERENCES Event_Type(TypeID),
   NotificationTime Text NOT NULL,
   PRIMARY KEY (ContactID, TypeID)
 );
@@ -57,17 +51,25 @@ CREATE TABLE Contact (
   PRIMARY KEY (ContactID, Contact)
 );
 
-
+DROP TABLE IF EXISTS Uses;
 CREATE TABLE Uses (
-  UserID REFERENCES User(UserID),
-  ContactID REFERENCES Contact(ContactID),
+  UserID INTEGER REFERENCES User(UserID),
+  ContactID INTEGER REFERENCES Contact(ContactID),
   PRIMARY KEY (UserID, ContactID)
 );
 
+DROP TABLE IF EXISTS Equipment;
 CREATE TABLE Equipment (
   EquipmentID Integer PRIMARY KEY,
-  LoanerID REFERENCES User(UserID),
-  BorrowerID REFERENCES User(UserID),
+  LoanerID INTEGER REFERENCES User(UserID),
+  BorrowerID INTEGER REFERENCES User(UserID),
   Kind Text,
   LoanDate DateTime DEFAULT (datetime('now', 'localtime'))
+);
+
+DROP TABLE IF EXISTS User;
+CREATE TABLE User (
+  UserID Integer PRIMARY KEY,
+  Name Text NOT NULL,
+  Password Text NOT NULL
 );
---
+++ insertvalues.sql
@@ -0,0 +1,23 @@
+INSERT INTO "Plays_For" VALUES(1,1,3,'Handler',1);
+INSERT INTO "Plays_For" VALUES(2,1,20,'Cutter',NULL);
+INSERT INTO "Plays_For" VALUES(3,1,4,'Handler',NULL);
+INSERT INTO "Plays_For" VALUES(4,2,10,'Defense',NULL);
+INSERT INTO "Plays_For" VALUES(5,2,14,'Striker',NULL);
+
+
+INSERT INTO "Coaches" VALUES(1,1);
+INSERT INTO "Coaches" VALUES(4,2);
+
+INSERT INTO "Team" VALUES(1,'Yak (TU Frisbee)');
+INSERT INTO "Team" VALUES(2,'Trojan Lacrosse');
+
+INSERT INTO "Event_Type" VALUES(1,'Game');
+INSERT INTO "Event_Type" VALUES(2,'Practice');
+INSERT INTO "Event_Type" VALUES(3,'Workout');
+INSERT INTO "Event_Type" VALUES(4,'Team Bonding Event');
+
+INSERT INTO "User" VALUES(1,'Joel Bierre','pass');
+INSERT INTO "User" VALUES(2,'Chris Shelor','pass');
+INSERT INTO "User" VALUES(3,'Todd Fenstermacher','pass');
+INSERT INTO "User" VALUES(4,'Mitch Pawlanta','pass');
+INSERT INTO "User" VALUES(5,'Mike Kammes','pass');
